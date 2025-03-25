72d074962190c1194896bf18cb707bc21d67cc9f
PheonixCS <destiny.sciense@gmail.com>
2025-03-25 11:08:35 +0300

    update vk auth

--- server/routes/api/vkAuth.js
+++ server/routes/api/vkAuth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const vkAuthService = require('../../services/vkAuthService');
-const config = require('../../config');
+const config = require('../../config/config');
 
 /**
  * Получение URL для авторизации в ВК
